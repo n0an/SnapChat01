551e92a8e1221dba3144e9c848c2e5b3487fd395
n0an <nantog@gmail.com>
2016-09-17 18:38:50 +0300

    Login/SignUp added

--- NagSnapChat/InboxViewController.swift
+++ NagSnapChat/InboxViewController.swift
@@ -7,8 +7,45 @@
 //
 
 import UIKit
+import Parse
 
 class InboxViewController: UITableViewController {
+    
+    struct Storyboard {
+        static let showLoginSegue = "Show Log In"
+    }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if PFUser.currentUser() == nil {
+            performSegueWithIdentifier(Storyboard.showLoginSegue, sender: nil)
+        }
+    }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBarHidden = false
+
+    }
+    
+    // MARK: - Target / Action
+    
+    @IBAction func logOutDidTap(sender: AnyObject) {
+        PFUser.logOut()
+        self.performSegueWithIdentifier(Storyboard.showLoginSegue, sender: nil)
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == Storyboard.showLoginSegue {
+            let loginSignUpVC = segue.destinationViewController as! LoginSignupViewController
+            
+            //!!! IMPORTANT
+            loginSignUpVC.hidesBottomBarWhenPushed = true
+            loginSignUpVC.navigationItem.hidesBackButton = true
+        }
+    }
     
 }
--- NagSnapChat/LoginSignupViewController.swift
+++ NagSnapChat/LoginSignupViewController.swift
@@ -16,9 +16,46 @@ class LoginSignupViewController: PFLogInViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "NagSnapChat"
+        self.navigationController?.navigationBarHidden = true
+        
+//        title = "NagSnapChat"
+        
+        let signUpVC = PFSignUpViewController()
+        signUpVC.delegate = self
+        self.delegate = self
+        
+        self.signUpController = signUpVC
     }
     
+    func showInbox() {
+        self.navigationController?.popToRootViewControllerAnimated(true)
+    }
     
 }
+
+
+extension LoginSignupViewController: PFSignUpViewControllerDelegate {
+    func signUpViewController(signUpController: PFSignUpViewController, didSignUpUser user: PFUser) {
+        dismissViewControllerAnimated(true, completion: nil)
+        showInbox()
+    }
+}
+
+
+extension LoginSignupViewController: PFLogInViewControllerDelegate {
+    func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
+        showInbox()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
